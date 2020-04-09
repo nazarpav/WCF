14cf05c23bcc44599e56c6772fce1f1f49a4092e
павлюк назар <hasab.k18@gmail.com>
2020-04-09 03:38:02 +0300

    3.37

--- HomeWork/18_04_2020/Client/MainWindow.xaml.cs
+++ HomeWork/18_04_2020/Client/MainWindow.xaml.cs
@@ -12,17 +12,30 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Client.Converter;
 
 namespace Client
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() => Convert());
+        }
+        private void Convert()
+        {
+            using (ConvertedClient convertedClient = new ConvertedClient())
+            {
+                ConvertedUnits convertedUnits = Dispatcher.Invoke(() =>  convertedClient.LinearMeasure(double.Parse(LinearMeasure_before.Text)));
+                Dispatcher.Invoke(()=>LinearMeasure_after.Text = "Yard: " + convertedUnits.yard + " | Foot: " + convertedUnits.foot + " | Inch: " + convertedUnits.inch);
+                Dispatcher.Invoke(() => FahrenheitToCelsius_after.Text = convertedClient.FahrenheitToCelsius(double.Parse(FahrenheitToCelsius_before.Text)).Celsius.ToString());
+                Dispatcher.Invoke(() => CelsiusToFahrenheit_after.Text = convertedClient.CelsiusToFahrenheit(double.Parse(CelsiusToFahrenheit_before.Text)).Fahrenheit.ToString());
+            }
         }
     }
 }
