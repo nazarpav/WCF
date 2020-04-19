723e1c8054b8f56f6bc1b8995f0b2f5f6a0730ee
unknown <hesab.k18@gmail.com>
2020-04-19 22:16:34 +0300

    Exam

---
+++ HomeWork/25_04_2020/ChatWindow/ChatWindow/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChatWindow.ServiceReference1;
+
+namespace ChatWindow
+{
+    public delegate void CallbackRecieve(string msg);
+    public delegate void CallbackUsersRefresh(string[] msg);
+    class CallbackHandler : IChatCallback
+    {
+        static public event CallbackRecieve OnRecieve;
+        static public event CallbackUsersRefresh OnUsersRefresh;
+        public void RecieveMessage(string msg)
+        {
+            OnRecieve(msg);
+        }
+
+        public void UsersRefresh(string[] msg)
+        {
+            OnUsersRefresh(msg);
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        static InstanceContext instance = new InstanceContext(new CallbackHandler());
+        ChatClient proxy = new ChatClient(instance);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CallbackHandler.OnRecieve += AddMessageToList;
+            CallbackHandler.OnUsersRefresh += UsersRefresh;
+        }
+
+        private void UsersRefresh(string[] users)
+        {
+            Users.Items.Clear();
+            foreach (var item in users)
+            {
+                Users.Items.Add(item);
+            }
+        }
+        // Login
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var res = proxy.Login(loginTb.Text);
+            if (res == null)
+                MessageBox.Show("User is already exist.");
+            foreach (var item in res)
+            {
+                Users.Items.Add(item);
+            }
+        }
+
+        // Send message
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            proxy.SendMessasge(loginTb.Text, msgTb.Text);
+
+            TextBlock tb = new TextBlock()
+            {
+                HorizontalAlignment = HorizontalAlignment.Right,
+                FontWeight = FontWeights.Bold,
+                Text = msgTb.Text
+            };
+            msgList.Items.Add(tb);
+        }
+
+        private void AddMessageToList(string msg)
+        {
+            msgList.Items.Add(msg);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            proxy.Logout(loginTb.Text);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            proxy.SendPrivate(loginTb.Text, msgTb.Text, private_name.Text);
+            TextBlock tb = new TextBlock()
+            {
+                HorizontalAlignment = HorizontalAlignment.Right,
+                FontWeight = FontWeights.Bold,
+                Text = "(Private to " + private_name.Text + ") " + msgTb.Text
+            };
+            msgList.Items.Add(tb);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            proxy.Logout(loginTb.Text);
+        }
+    }
+}
