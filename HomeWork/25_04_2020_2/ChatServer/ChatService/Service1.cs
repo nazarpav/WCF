63672c332701dd5ed3d02864bccbd69ab0fd5ae9
nazar pavliuk <hesab.k18@gmail.com>
2020-04-25 23:44:35 +0300

    25_04_2020

---
+++ HomeWork/25_04_2020_2/ChatServer/ChatService/Service1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class Chat : IChat
+    {
+        private Dictionary<string, ICallback> clients = new Dictionary<string, ICallback>();
+        public void SendMessasge(string userName, string msg)
+        {
+            foreach (var item in clients)
+            {
+                if (item.Key == userName)
+                    continue;
+                item.Value.RecieveMessage(userName + ": " + msg);
+            }
+        }
+
+        public void Logout(string name)
+        {
+            clients.Remove(name);
+        }
+
+        string[] IChat.Login(string name)
+        {
+            if (clients.ContainsKey(name))
+            {
+                return null;
+            }
+            clients.Add(name, OperationContext.Current.GetCallbackChannel<ICallback>());
+            foreach (var item in clients)
+            {
+                if (item.Key != name)
+                {
+                    item.Value.UsersRefresh(clients.Keys.ToArray());
+                }
+            }
+            return clients.Keys.ToArray();
+        }
+    }
+}
---
+++ HomeWork/25_04_2020_2/ChatWindow/ChatWindow/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChatWindow.ServiceReference1;
+
+namespace ChatWindow
+{
+    public delegate void CallbackRecieve(string msg);
+    public delegate void CallbackUsersRefresh(string[] msg);
+    class CallbackHandler : IChatCallback
+    {
+        static public event CallbackRecieve OnRecieve;
+        static public event CallbackUsersRefresh OnUsersRefresh;
+        public void RecieveMessage(string msg)
+        {
+            OnRecieve(msg);
+        }
+
+        public void UsersRefresh(string[] msg)
+        {
+            OnUsersRefresh(msg);
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        static InstanceContext instance = new InstanceContext(new CallbackHandler());
+        ChatClient proxy = new ChatClient(instance);
+        bool isLogin = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CallbackHandler.OnRecieve += AddMessageToList;
+            CallbackHandler.OnUsersRefresh += UsersRefresh;
+        }
+
+        private void UsersRefresh(string[] users)
+        {
+            Users.Items.Clear();
+            foreach (var item in users)
+            {
+                Users.Items.Add(item);
+            }
+        }
+        // Login
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var res = proxy.Login(loginTb.Text);
+            if (res == null)
+                MessageBox.Show("User is already exist.");
+            foreach (var item in res)
+            {
+                Users.Items.Add(item);
+            }
+            isLogin = true;
+        }
+
+        // Send message
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(!isLogin)
+            {
+                MessageBox.Show("Login!!");
+                return;
+            }
+            proxy.SendMessasge(loginTb.Text, msgTb.Text);
+
+            TextBlock tb = new TextBlock()
+            {
+                HorizontalAlignment = HorizontalAlignment.Right,
+                FontWeight = FontWeights.Bold,
+                Text = msgTb.Text
+            };
+            msgList.Items.Add(tb);
+        }
+
+        private void AddMessageToList(string msg)
+        {
+            msgList.Items.Add(msg);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            proxy.Logout(loginTb.Text);
+            isLogin = false;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            proxy.Logout(loginTb.Text);
+        }
+    }
+}
